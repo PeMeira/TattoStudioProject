08fed61ceb4768aee1d2a282bd7baec69d98a441
PeMeira <phsmeira00@gmail.com>
2025-04-29 17:42:54 -0300

    sistema de busca

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,42 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const dropdownToggles = document.querySelectorAll('[data-dropdown-toggle]');
-  
-    dropdownToggles.forEach(toggle => {
-      toggle.addEventListener('click', function (e) {
-        e.preventDefault();
-        const parent = this.closest('.dropdown-item');
-  
-        document.querySelectorAll('.dropdown-item.active').forEach(item => {
-          if (item !== parent) {
-            item.classList.remove('active');
+document.addEventListener('DOMContentLoaded', function(){
+  const dropdownToggle = document.querySelectorAll('[data-dropdown-toggle]');
+  dropdownToggle.forEach((toggle) => {
+      toggle.addEventListener('click', function() {
+          const parent = this.closest('.dropdown-item');
+          parent.classList.toggle('active');
+      })
+  })
+
+  document.addEventListener('click', function (e) {
+      dropdownToggle.forEach((toggle) => {
+          const parent = toggle.closest('.dropdown-item');
+          if(!parent.contains(e.target)) {
+              parent.classList.remove('active');
           }
-        });
-  
-        parent.classList.toggle('active');
-      });
-    });
-  
-    document.addEventListener('click', function (e) {
-      const isClickInside = e.target.closest('.dropdown-item');
-      if (!isClickInside) {
-        document.querySelectorAll('.dropdown-item.active').forEach(item => {
-          item.classList.remove('active');
-        });
-      }
-    });
-  });
+      })
+  })
+})
+
+const searchToggle = document.getElementById('searchToggle');
+const searchOverlay = document.getElementById('searchOverlay');
+const closeSearch = document.getElementById('closeSearch');
+
+searchToggle.addEventListener('click', function () {
+  searchOverlay.style.display = 'flex';
+  setTimeout(() => {
+    searchOverlay.querySelector('search-input').focus();
+  }, 100);
+});
+
+// fechar o botão 
+
+closeSearch.addEventListener('click', () => {
+  searchOverlay.style.display = 'none';
+})
+
+// fechar com ESC
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    searchOverlay.style.display = 'none';
+  }
+})
